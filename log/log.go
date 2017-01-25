9800b7501d2f1ecec2b0f59e088df68bd8bb3e67
pivaldi <contact@piprime.fr>
2017-01-25 18:40:22 +0100

    Make logger not a direct logrus instance

--- log/log.go
+++ log/log.go
@@ -6,9 +6,17 @@ import (
 	"github.com/Sirupsen/logrus"
 )
 
+// Logger is the Ovya logger, wrapping logrus.Logger
+// to ovoid app logrus hard dependency
+type Logger struct {
+	*logrus.Logger
+}
+
 // NewConsoleLogger is a constructor for a Logrus text based formatter
-func NewConsoleLogger() (log *logrus.Logger) {
-	log = logrus.New()
+func NewConsoleLogger() (log *Logger) {
+	log = &Logger{}
+	log.Logger = logrus.New()
+
 	log.Formatter = &formatter.ConsoleFormatter{
 		logrus.TextFormatter{
 			TimestampFormat: "2006-01-02 15:04:05",
