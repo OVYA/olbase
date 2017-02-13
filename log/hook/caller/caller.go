6bd9f39759b3c5fc1bb970fe9e66ebf1049ae91c
pivaldi <contact@piprime.fr>
2017-02-13 12:35:36 +0100

    Add caller hook from official logrus git repos

---
+++ log/hook/caller/caller.go
@@ -0,0 +1,37 @@
+package logruscaller
+
+import (
+	"path/filepath"
+	"runtime"
+	"strconv"
+	"strings"
+
+	"github.com/Sirupsen/logrus"
+)
+
+type CallerHook struct {
+}
+
+func (hook *CallerHook) Fire(entry *logrus.Entry) error {
+	entry.Data["caller"] = hook.caller()
+	return nil
+}
+
+func (hook *CallerHook) Levels() []logrus.Level {
+	return []logrus.Level{
+		logrus.PanicLevel,
+		logrus.FatalLevel,
+		logrus.ErrorLevel,
+		logrus.WarnLevel,
+		logrus.InfoLevel,
+		logrus.DebugLevel,
+	}
+}
+
+func (hook *CallerHook) caller() string {
+	if _, file, line, ok := runtime.Caller(6); ok {
+		return strings.Join([]string{filepath.Base(file), strconv.Itoa(line)}, ":")
+	}
+	// not sure what the convention should be here
+	return ""
+}
---
+++ log/hook/caller/doc.go
@@ -0,0 +1,3 @@
+// Package logruscaller includes caller=<file>:<line> in log entries.
+// Comme from https://github.com/sirupsen/logrus/ on branch caller-hook
+package logruscaller
